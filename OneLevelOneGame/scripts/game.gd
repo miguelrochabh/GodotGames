8543f6d6040a55e59e1a7be729d392abfe0277d2
miguelrochabh <rocha.a.miguel@gmail.com>
2021-02-09 16:00:51 -0300

    Mais espinhos adicionados

--- OneLevelOneGame/scripts/game.gd
+++ OneLevelOneGame/scripts/game.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+	
+func _on_Espinho_body_entered(body):
+	morrer()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func morrer():
+	get_node("Personagem").set_position(Vector2(17, -71))
